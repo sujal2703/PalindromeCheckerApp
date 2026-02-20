a006c84581c97a1ea0312bcbef8e8ae040ec5073
your GitHub username <your GitHub email>
2026-02-20 10:24:54 +0530

    UC4:Character Array Based Palindrome Check

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,15 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // Display Application Name
+        System.out.println("------------------------------------");
+        System.out.println("   WELCOME TO PALINDROME CHECKER    ");
+        System.out.println("------------------------------------");
+
+        // Display Application Version
+        System.out.println("Application Version: 1.0.0");
+        System.out.println("Status: System Initialized...");
+
+        // Program exits after this flow
+        System.out.println("------------------------------------");
+    }
+}
